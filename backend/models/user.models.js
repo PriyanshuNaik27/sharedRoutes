6d10270ba43fde242318799326a2735090dcc3b2
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-08-03 14:06:43 +0530

    added bcypt , to hash and compare password

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcyrpt from "bcrypt"
+
 const userSchema= new mongoose.Schema(
     {
        name: {
@@ -15,11 +17,29 @@ const userSchema= new mongoose.Schema(
        password:{
         required:true,
         type:String,
-        minLength:6,
-        maxLength:20
        }
 
     }
  ,{timestamps:true});
+
+ // hash the password before saving and before saving check if the passwrod is modified or not;
+userSchema.pre("save", async function(next){
+   if(!this.isModified('password')){
+      return next();
+   }
+   try{
+      this.password= await  bcyrpt.hash(password,10);
+      next();
+   }catch(error){
+      console.error("error while hashing the password",error);
+      next(error);
+   }
+});
+
+//method to see if the password is correct or not
+userSchema.methods.isPasswordCorrect() = async function(password){
+   return await bcyrpt.compare(password,this.password);
+}
+
  
  export const User= mongoose.model("User",userSchema);
