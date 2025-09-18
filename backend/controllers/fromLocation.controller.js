9c145c1f7bcb62a52c27ea78e58125505aef8481
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-09-18 14:29:22 +0530

    added logic for adding a locatin

---
+++ backend/controllers/fromLocation.controller.js
@@ -0,0 +1,42 @@
+import { FromLocation } from "../models/fromLocation.model.js";
+import slugify from "slugify";
+export const createLocation = async(req,res) =>{
+    try{
+        const {locationName} = req.body;
+
+
+        //this is from where i used slugify 
+        //https://www.npmjs.com/package/slugify?activeTab=readme
+
+        const locationSlug =slugify(locationName,{
+            lower:true,
+            strict:true,
+            trim:true
+        });
+        const userId = req.user._id 
+        if(await FromLocation.findOne({locationSlug})){
+            return res.status(409).json({
+                message:"location already exits bro , please search"
+            })
+        }
+
+        
+        const newLocation = await FromLocation.create({
+            locationName : locationName,
+            locationSlug : locationSlug,
+            uploadedBy : userId,
+            
+        })
+
+        return res.status(201).json({
+            message: "Location created successfully",
+            location: newLocation,
+        });
+
+    }catch(err){
+        res.status(500).json({
+            message:"error while creating the location ",
+            error : err.message
+        })
+    }
+}
