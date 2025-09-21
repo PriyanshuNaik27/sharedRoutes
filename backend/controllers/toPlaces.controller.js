e66f1d4dd40a87f52036f0b5fe2b97db9b05375c
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-09-21 16:09:13 +0530

    controller -> getAllPlaces

--- backend/controllers/toPlaces.controller.js
+++ backend/controllers/toPlaces.controller.js
@@ -5,16 +5,29 @@ export const getAllPlaces = async(req,res)=>{
     try {
         const {locationSlug} = req.params;
 
-        const doesExist = FromLocation.findOne({locationSlug});
+        const doesExist = await FromLocation.findOne({locationSlug});
         if(!doesExist){
             return res.status(400).json({
                 message:" cannot find the location in our database , first add the location "
             })
         }
 
-        const places = FromLocation.fin
+        //learn about populate and its importance from here 
+        //https://www.geeksforgeeks.org/mongodb/mongoose-populate-method/
+
+        const places = await toPlaces.find({fromLocation: doesExist._id})
+                        .populate("fromLocation");
+
+        return res.status(200).json({
+        message: "Places fetched successfully",
+        data: places
+        });
+
 
     } catch (error) {
-        
+        return res.status(500).json({
+        message: "Server error",
+        error: error.message
+        });
     }
-}
+};
