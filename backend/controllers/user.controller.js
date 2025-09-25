2b3e58acf43d83d35869d1078caaaa2649661182
PriyanshuNaik27 <priyanshunaik143@gmail.com>
2025-09-25 17:54:10 +0530

    controller -> logout user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import {  z } from "zod";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcrypt";
 
@@ -12,7 +12,7 @@ const generateAccessAndRefreshToken = async (userId) => {
     await user.save({ validateBeforeSave: false });
     return { refreshToken, accessToken };
   } catch (error) {
-    console.error("error in token gen " ,error.message);
+    console.error("error in token gen ", error.message);
     throw error;
   }
 };
@@ -31,9 +31,19 @@ export const registerUser = async (req, res) => {
 
 */
   const requireBody = z.object({
-    email: z.string().email({ message: "Invalid email format" }).min(3, { message: "Email is too short" }).max(100, { message: "Email is too long" }),
-    password: z.string().min(1, { message: "Password is required" }).max(50, { message: "Password is too long" }),
-    userName: z.string().min(5, { message: "Username must be at least 5 characters" }).max(50, { message: "Username is too long" }),
+    email: z
+      .string()
+      .email({ message: "Invalid email format" })
+      .min(3, { message: "Email is too short" })
+      .max(100, { message: "Email is too long" }),
+    password: z
+      .string()
+      .min(1, { message: "Password is required" })
+      .max(50, { message: "Password is too long" }),
+    userName: z
+      .string()
+      .min(5, { message: "Username must be at least 5 characters" })
+      .max(50, { message: "Username is too long" }),
   });
 
   const parsedData = requireBody.safeParse(req.body);
@@ -50,9 +60,9 @@ export const registerUser = async (req, res) => {
     try {
       const { email, password, userName } = req.body;
 
-      const existedUser = await User.findOne( {email} );
+      const existedUser = await User.findOne({ email });
       if (existedUser) {
-        console.error("user exits")
+        console.error("user exits");
         return res.json({
           status: 404,
           message: "user is already exits",
@@ -136,5 +146,41 @@ export const loginUser = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logoutUser = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    await User.findByIdAndUpdate(
+      userId,
+      {
+        $set: {
+          refreshToken: undefined,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    return res
+      .status(200)
+      .clearCookie("accessToken", options)
+      .clearCookie("refreshToken", options)
+      .json({
+        success: true,
+        message: "user logout succesfulyy",
+      });
+  } catch (error) {
+    console.error("Logout error:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong while logging out.",
+    });
   }
 };
